ef34a26672451be28f3e473289133bf4056b44a9
R1cegg <1846203466@qq.com>
2026-06-25 10:58:38 +0800

    demo.2

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def print_hi(name):
 
 
 # 按装订区域中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == '__main__':ee
+    print_hi('PyCharm')ee
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
